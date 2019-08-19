6ae78d9f308da659fbd719ff6a88556f810d53db
Junphill Hwang <444.gkk@gmail.com>
2019-08-19 23:44:55 +0900

    skill, repository model 수정

--- manage_app/models.py
+++ manage_app/models.py
@@ -24,10 +24,12 @@ class Career(models.Model):
 class Skills(models.Model):
     title = models.CharField(default=None, null=True, blank=True, max_length=100)
     point = models.IntegerField(default=None, null=True, blank=True)
+    link = models.URLField(default=None, null=True, blank=True)
     icon = models.CharField(default=None, null=True, blank=True, max_length=100)
 
 
 class Repository(models.Model):
+    type = models.CharField(blank=True, null=True, default=None, max_length=10)
     title = models.CharField(blank=True, null=True, max_length=50)
     text = MarkdownxField(default=None)
     published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
@@ -36,6 +38,12 @@ class Repository(models.Model):
     period = models.CharField(default=None, null=True, blank=True, max_length=50)
     technique = models.TextField(default=None, null=True, blank=True)
     role = models.TextField(default=None, null=True, blank=True)
+
+    img1 = models.ImageField(default=None, null=True, blank=True, upload_to='img/repository/%Y/%m/%d')
+    img2 = models.ImageField(default=None, null=True, blank=True, upload_to='img/repository/%Y/%m/%d')
+    img3 = models.ImageField(default=None, null=True, blank=True, upload_to='img/repository/%Y/%m/%d')
+    img4 = models.ImageField(default=None, null=True, blank=True, upload_to='img/repository/%Y/%m/%d')
+    img5 = models.ImageField(default=None, null=True, blank=True, upload_to='img/repository/%Y/%m/%d')
 
     def formatted_markdown(self):
         return markdownify(self.text)
