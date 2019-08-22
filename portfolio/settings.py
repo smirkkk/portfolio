6ff3a3aeee4ce8972e936cfacaef51d95d5eb695
Junphill Hwang <444.gkk@gmail.com>
2019-08-22 17:30:34 +0900

    no message

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from datetime import datetime
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
